8f3d488ee4cc9969f1436c8e99d06ad47aa68085
Kunjan Kshetri <kunjan@kshetri.com>
2018-06-28 21:28:10 -0400

    adding readme

--- cpp/CPPCraftDemo/CPPCraftDemo/CPPCraftDemo.cpp
+++ cpp/CPPCraftDemo/CPPCraftDemo/CPPCraftDemo.cpp
@@ -1,10 +1,3 @@
-/**
-    We provide a simple base implementation of a Record datastructure and collection of Records
-    We would like to implement FindMatchingRecords function that searches through the records for a string
-    We have provided a base FindMatchingRecords Function that we would like to optimize
-    Please provide your own implementation of Record, RecordCollection and FindMatchingRecords
-    Feel free to change the profiler function, add unit tests, move the definitions into separate classes.
-*/
 #include "stdafx.h"
 #include <string>
 #include <vector>
