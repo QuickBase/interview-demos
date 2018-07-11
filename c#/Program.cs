cace7417b0129bbcbf7028753dc3346124815b2e
bobbravo2 <rgregor@quickbase.com>
2018-07-11 11:08:13 -0400

    added C# props @drewditto

---
+++ c#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+            Console.WriteLine("Getting DB Connection...");
+
+            IDbManager db = new SqliteDbManager();
+            DbConnection conn = db.getConnection();
+
+            if(conn == null)
+            {
+                Console.WriteLine("Failed to get connection");
+            }
+        }
+    }
+}
---
+++ c#/SqliteDbManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class SqliteDbManager : IDbManager
+    {
+        public DbConnection getConnection()
+        {
+            try
+            {
+                var connection = new SQLiteConnection("Data Source=citystatecountry.db;Version=3;FailIfMissing=True");
+                return connection.OpenAndReturn();
+            }
+            catch(SQLiteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
