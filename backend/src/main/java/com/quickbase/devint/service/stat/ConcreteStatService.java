50f6fb9d518397251c41718221b0df8bb182a55e
Viktor Velchev <22957914+zilopbg@users.noreply.github.com>
2024-03-30 22:54:32 +0200

    Refactor ConcreteStatService

--- backend/src/main/java/com/quickbase/devint/service/stat/ConcreteStatService.java
+++ backend/src/main/java/com/quickbase/devint/service/stat/ConcreteStatService.java
@@ -8,43 +8,42 @@ import java.util.List;
 
 public class ConcreteStatService implements StatService {
 
-	@Override
 	/**
 	 * Returns an unordered list of countries and their populations
 	 */
+	@Override
 	public List<Pair<String, Integer>> getCountryPopulations() {
-		List<Pair<String, Integer>> output = new ArrayList<Pair<String, Integer>>();
+		List<Pair<String, Integer>> output = new ArrayList<>();
 		
 		// Pretend this calls a REST API somewhere
-		output.add(new ImmutablePair<String, Integer>("India",1182105000));
-		output.add(new ImmutablePair<String, Integer>("United Kingdom",62026962));
-		output.add(new ImmutablePair<String, Integer>("Chile",17094270));
-		output.add(new ImmutablePair<String, Integer>("Mali",15370000));
-		output.add(new ImmutablePair<String, Integer>("Greece",11305118));
-		output.add(new ImmutablePair<String, Integer>("Armenia",3249482));
-		output.add(new ImmutablePair<String, Integer>("Slovenia",2046976));
-		output.add(new ImmutablePair<String, Integer>("Saint Vincent and the Grenadines",109284));
-		output.add(new ImmutablePair<String, Integer>("Bhutan",695822));
-		output.add(new ImmutablePair<String, Integer>("Aruba (Netherlands)",101484));
-		output.add(new ImmutablePair<String, Integer>("Maldives",319738));
-		output.add(new ImmutablePair<String, Integer>("Mayotte (France)",202000));
-		output.add(new ImmutablePair<String, Integer>("Vietnam",86932500));
-		output.add(new ImmutablePair<String, Integer>("Germany",81802257));
-		output.add(new ImmutablePair<String, Integer>("Botswana",2029307));
-		output.add(new ImmutablePair<String, Integer>("Togo",6191155));
-		output.add(new ImmutablePair<String, Integer>("Luxembourg",502066));
-		output.add(new ImmutablePair<String, Integer>("U.S. Virgin Islands (US)",106267));
-		output.add(new ImmutablePair<String, Integer>("Belarus",9480178));
-		output.add(new ImmutablePair<String, Integer>("Myanmar",59780000));
-		output.add(new ImmutablePair<String, Integer>("Mauritania",3217383));
-		output.add(new ImmutablePair<String, Integer>("Malaysia",28334135));
-		output.add(new ImmutablePair<String, Integer>("Dominican Republic",9884371));
-		output.add(new ImmutablePair<String, Integer>("New Caledonia (France)",248000));
-		output.add(new ImmutablePair<String, Integer>("Slovakia",5424925));
-		output.add(new ImmutablePair<String, Integer>("Kyrgyzstan",5418300));
-		output.add(new ImmutablePair<String, Integer>("Lithuania",3329039));
-		output.add(new ImmutablePair<String, Integer>("United States of America",309349689));
+		output.add(new ImmutablePair<>("India",1182105000));
+		output.add(new ImmutablePair<>("United Kingdom",62026962));
+		output.add(new ImmutablePair<>("Chile",17094270));
+		output.add(new ImmutablePair<>("Mali",15370000));
+		output.add(new ImmutablePair<>("Greece",11305118));
+		output.add(new ImmutablePair<>("Armenia",3249482));
+		output.add(new ImmutablePair<>("Slovenia",2046976));
+		output.add(new ImmutablePair<>("Saint Vincent and the Grenadines",109284));
+		output.add(new ImmutablePair<>("Bhutan",695822));
+		output.add(new ImmutablePair<>("Aruba (Netherlands)",101484));
+		output.add(new ImmutablePair<>("Maldives",319738));
+		output.add(new ImmutablePair<>("Mayotte (France)",202000));
+		output.add(new ImmutablePair<>("Vietnam",86932500));
+		output.add(new ImmutablePair<>("Germany",81802257));
+		output.add(new ImmutablePair<>("Botswana",2029307));
+		output.add(new ImmutablePair<>("Togo",6191155));
+		output.add(new ImmutablePair<>("Luxembourg",502066));
+		output.add(new ImmutablePair<>("U.S. Virgin Islands (US)",106267));
+		output.add(new ImmutablePair<>("Belarus",9480178));
+		output.add(new ImmutablePair<>("Myanmar",59780000));
+		output.add(new ImmutablePair<>("Mauritania",3217383));
+		output.add(new ImmutablePair<>("Malaysia",28334135));
+		output.add(new ImmutablePair<>("Dominican Republic",9884371));
+		output.add(new ImmutablePair<>("New Caledonia (France)",248000));
+		output.add(new ImmutablePair<>("Slovakia",5424925));
+		output.add(new ImmutablePair<>("Kyrgyzstan",5418300));
+		output.add(new ImmutablePair<>("Lithuania",3329039));
+		output.add(new ImmutablePair<>("United States of America",309349689));
 		return output;
 	}
-
 }
